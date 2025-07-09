31d88fa4ebc0b8dd8df3b0c2abdd39adcc71f6db
uyadav <utyadav911@gmail.com>
2025-07-09 16:40:07 -0600

    653B

---
+++ 653/B.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int calc(long long n) {
+	int cnt_2, cnt_3;
+	while (n%2 == 0) {
+		n = n/2;
+		cnt_2++;
+	}
+	while (n%3 == 0) {
+		n = n/3;
+		cnt_3++;
+	}
+	if (n > 1 || cnt_2 > cnt_3) return -1;
+	else return (cnt_3 - cnt_2) + cnt_3;
+}
+int main() {
+	int t;
+	cin >> t;
+	long long n;
+	for (int i=0; i<t; i++) {
+		cin >> n;
+		cout << calc(n) << "\n";
+	}
+}
