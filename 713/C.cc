43f0da7053f2c21891fc4e26dd27555a70d7f327
uyadav <utyadav911@gmail.com>
2025-07-10 02:18:19 -0600

    fuck fuck fuck

--- 713/C.cc
+++ 713/C.cc
@@ -4,8 +4,66 @@ using namespace std;
 int main() {
 	int t;
 	cin >> t;
-	int a, b;
-	string str;
-	for (int i=0; i<t; i++) {
+	while (t--) {
+		int a, b;
 		cin >> a >> b;
-		cin >> str;
+		string s;
+		cin >> s;
+		int n = a + b;
+
+		int cnt0 = count(s.begin(), s.end(), '0');
+		int cnt1 = count(s.begin(), s.end(), '1');
+		a -= cnt0;
+		b -= cnt1;
+
+		bool bad = false;
+
+		for (int i = 0; i < n/2; i++) {
+			int j = n - i - 1;
+			if (s[i] != '?' && s[j] == '?') {
+				s[j] = s[i];
+				if (s[i] == '0') a--;
+				else b--;
+			} else if (s[i] == '?' && s[j] != '?') {
+				s[i] = s[j];
+				if (s[j] == '0') a--;
+				else b--;
+			} else if (s[i] != '?' && s[j] != '?' && s[i] != s[j]) {
+				bad = true;
+			}
+		}
+
+		if (n % 2 == 1 && s[n/2] == '?') {
+			if (a % 2 == 1) {
+				s[n/2] = '0';
+				a--;
+			} else {
+				s[n/2] = '1';
+				b--;
+			}
+		}
+
+		for (int i = 0; i < n/2; i++) {
+			int j = n - i - 1;
+			if (s[i] == '?' && s[j] == '?') {
+				if (a >= 2) {
+					s[i] = s[j] = '0';
+					a -= 2;
+				} else if (b >= 2) {
+					s[i] = s[j] = '1';
+					b -= 2;
+				} else {
+					bad = true;
+					break;
+				}
+			}
+		}
+
+		if (a < 0 || b < 0 || bad) {
+			cout << -1 << "\n";
+		} else {
+			cout << s << "\n";
+		}
+	}
+}
+
